9a6c5919216237a1d4fedf5e9d7a69506f9726c1
Windover <WINDOJ9@TDBFG.COM>
2017-03-24 16:25:54 -0400

    - Updating Elpy mode to not use flymake  - trying to restructure custom packages

--- config/custom.el
+++ config/custom.el
@@ -98,7 +98,26 @@
  '(org-insert-mode-line-in-empty-file t)
  '(package-selected-packages
 	 (quote
-		(fsharp-mode markdown-mode elpy highlight-indent-guides js2-mode web-mode ## rainbow-mode lua-mode fish-mode circe powershell centered-cursor-mode yaml-mode monokai-theme smart-tabs-mode)))
+		(toml-mode
+		 rust-mode
+		 hl-line+
+		 fsharp-mode
+		 markdown-mode
+		 elpy
+		 highlight-indent-guides
+		 js2-mode
+		 web-mode
+		 ##
+		 rainbow-mode
+		 lua-mode
+		 fish-mode
+		 circe
+		 powershell
+		 centered-cursor-mode
+		 yaml-mode
+		 monokai-theme
+		 smart-tabs-mode)
+		))
  '(php-mode-coding-style (quote wordpress))
  '(pos-tip-background-color "#A6E22E")
  '(pos-tip-foreground-color "#272822")
--- config/python.el
+++ config/python.el
@@ -1,3 +1,4 @@
 ;; Python mode hook(when (require 'flycheck nil t)
 (add-hook 'elpy-mode-hook 'flycheck-mode)
+(add-hook 'elpy-mode-hook (lambda () (flymake-mode -1)))
 (add-hook 'python-mode-hook 'elpy-mode)
